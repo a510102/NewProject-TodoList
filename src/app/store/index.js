d83e14c1913fc5de7432163c00647ff27a5b069d
jay <b98310047@hotmail.com>
2021-06-15 23:04:25 +0800

    [ADD] add page on root store

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import homeSlice from '../Pages/Home/slice';
+import pageSlice from '../components/Pagination/slice';
 
 
 export const store = configureStore({
   reducer: {
-    homeSlice
+    homeSlice,
+    page: pageSlice,
   }
 });
