b97708bd0b71c1859e34c8a684fee496e56278bf
jay <b98310047@hotmail.com>
2021-06-15 23:05:39 +0800

    [UPDATE] update the wrapcontainer layout

--- src/app/styles/WrapContainer/index.js
+++ src/app/styles/WrapContainer/index.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 import { space, layout, color } from 'styled-system';
 
-const WrapContainer = styled.div(
-  {
-    width: '100vw',
-    height: '100vh',
-  },
-  color,
-  space,
-  layout
-);
+const WrapContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  ${color}
+  ${space}
+  ${layout}
+`;
 
 export default WrapContainer;
